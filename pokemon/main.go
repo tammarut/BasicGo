feda9a2cb67e76606a27b7d20df4e5e9076e10fa
tammarut <tammarut.nua@gmail.com>
2019-01-17 18:13:21 +0700

    pokemon: first tutorial =>work!! and pokemondb.txt

---
+++ pokemon/main.go
@@ -0,0 +1,66 @@
+package main
+
+//! Go Connect Mongodb | Tutorial
+//* Pokemondb
+import (
+	"fmt"
+	"log"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Pokemon struct {
+	ID      string `bson:"_id"`
+	Name    string
+	Element string
+	Weight  string
+}
+
+func getPokemons() []Pokemon {
+	session, err := mgo.Dial("mongodb://127.0.0.1:27017/pokemondb")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("pokemondb").C("pokemons")
+	pokemons := []Pokemon{}
+	err = c.Find(nil).All(&pokemons)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return pokemons
+}
+
+func getPokemon(id string) Pokemon {
+	session, err := mgo.Dial("mongodb://127.0.0.1:27017/pokemondb")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("pokemondb").C("pokemons")
+	pokemon := Pokemon{}
+	err = c.Find(bson.M{"_id": id}).One(&pokemon)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return pokemon
+}
+
+func main() {
+	pokemons := getPokemons()
+	fmt.Println(pokemons)
+
+	fmt.Println("-------------------------------------------")
+
+	pokemon := getPokemon("002")
+	fmt.Println(pokemon)
+}
