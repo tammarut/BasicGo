52aa01fa804100c92b31adb587281280efb78882
tammarut <tammarut>
2019-02-02 18:41:15 +0700

    ninjalv13[functions.go]: make easy mockslice =>Example, Benchmark

--- amateur/ninja_lv13/03/mymath/functions_test.go
+++ amateur/ninja_lv13/03/mymath/functions_test.go
@@ -27,25 +27,15 @@ func TestCenteredAvg(t *testing.T) {
 }
 
 func ExampleCenteredAvg() {
-	box := []blueprint{
-		blueprint{[]int{1, 4, 6, 8, 100}, 6},
-		blueprint{[]int{0, 8, 10, 1000}, 9},
-		blueprint{[]int{9000, 4, 10, 8, 6, 12}, 9},
-		blueprint{[]int{123, 744, 140, 200}, 170},
-	}
+	solo := []int{1, 4, 6, 8, 100}
+	fmt.Println(CenteredAvg(solo))
+	//Output:
+	//6
 
-	for _, v := range box {
-		fmt.Println(CenteredAvg(v.data))
-		//Output:
-		//6
-		//9
-		//9
-		//170
-	}
 }
 
 func BenchmarkCenteredAvg(b *testing.B) {
-	solo := []int{6, 2, 15, 8, 5}
+	solo := []int{1, 4, 6, 8, 100}
 	for i := 0; i < b.N; i++ {
 		CenteredAvg(solo)
 	}
