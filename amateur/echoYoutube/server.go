248c92805e9aa144a9c49aa9322185722d39fb09
tammarut <tammarut>
2019-02-09 01:08:29 +0700

    echoYoutube: GET by parameter and can select string, json

--- amateur/echoYoutube/server.go
+++ amateur/echoYoutube/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -10,6 +11,27 @@ import (
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, echoYoutube!")
+}
+
+func getCats(c echo.Context) error {
+	mobile := c.QueryParam("mobile")
+	storage := c.QueryParam("storage")
+	typee := c.QueryParam("type")
+
+	dataType := c.Param("data")
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("Mobile: %s\nstorage: %s\ntype: %s\n", mobile, storage, typee))
+	}
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"mobile":  mobile,
+			"storage": storage,
+			"type":    typee,
+		})
+	}
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"Error": "We don't know you want Json or string",
+	})
 }
 
 func main() {
@@ -22,6 +44,7 @@ func main() {
 
 	// Routes
 	e.GET("/", hello)
+	e.GET("/cats/:data", getCats)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
