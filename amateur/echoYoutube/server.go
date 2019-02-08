782fa4861749283b9a384726001c383752cb5cca
tammarut <tammarut>
2019-02-09 01:15:52 +0700

    echoYoutube: comment explain code.

--- amateur/echoYoutube/server.go
+++ amateur/echoYoutube/server.go
@@ -1,5 +1,6 @@
 package main
 
+//! Golang API(echo) =>> learn from Youtube
 import (
 	"fmt"
 	"net/http"
@@ -9,27 +10,27 @@ import (
 )
 
 // Handler
-func hello(c echo.Context) error {
+func hello(c echo.Context) error { //=> Default
 	return c.String(http.StatusOK, "Hello, echoYoutube!")
 }
 
 func getCats(c echo.Context) error {
-	mobile := c.QueryParam("mobile")
-	storage := c.QueryParam("storage")
-	typee := c.QueryParam("type")
+	mobile := c.QueryParam("mobile")   //=> receive from param
+	storage := c.QueryParam("storage") //=> receive from param
+	typee := c.QueryParam("type")      //=> receive from param
 
-	dataType := c.Param("data")
-	if dataType == "string" {
+	dataType := c.Param("data") //=> what datatype you want [string, json]
+	if dataType == "string" {   //=> if 'string': return c.String
 		return c.String(http.StatusOK, fmt.Sprintf("Mobile: %s\nstorage: %s\ntype: %s\n", mobile, storage, typee))
 	}
-	if dataType == "json" {
+	if dataType == "json" { //=> if 'json': return map[string] string
 		return c.JSON(http.StatusOK, map[string]string{
 			"mobile":  mobile,
 			"storage": storage,
 			"type":    typee,
 		})
 	}
-	return c.JSON(http.StatusBadRequest, map[string]string{
+	return c.JSON(http.StatusBadRequest, map[string]string{ //=> return Error Unknown type
 		"Error": "We don't know you want Json or string",
 	})
 }
@@ -43,8 +44,8 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/", hello)
-	e.GET("/cats/:data", getCats)
+	e.GET("/", hello)             //=> Hello
+	e.GET("/cats/:data", getCats) //=> GET by parameter
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
