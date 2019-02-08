a0a72335c182e181a2ac599a6b8dddc243c293dd
tammarut <tammarut>
2019-02-08 17:44:02 +0700

    goroutine: comment explain code.

--- amateur/exercise/goroutine/main.go
+++ amateur/exercise/goroutine/main.go
@@ -5,39 +5,39 @@ import (
 	"time"
 )
 
-func buyGkassesMinimart() {
+func buyGkassesMinimart() { //=> ซื้อแว่นตา
 	time.Sleep(1 * time.Second)
 	fmt.Println("ซื้อแว่นตา ที่มินิมาท")
 }
-func buyWatchesCentral() {
+func buyWatchesCentral() { //=> ซื้อนาฬิกา
 	time.Sleep(1 * time.Second)
 	fmt.Println("ซื้อนาฬิกา ที่เซนทรัล")
 }
-func buyFruitsParagon() {
+func buyFruitsParagon() { //=> ซื้อผลไม้
 	time.Sleep(1 * time.Second)
 	fmt.Println("ซื้อผลไม้ ที่พารากอน")
 }
-func buyCarHonda() {
+func buyCarHonda() { //=> ซื้อรถ
 	time.Sleep(1 * time.Second)
 	fmt.Println("ซื้อรถ ที่ฮอนด้า")
 }
-func sendToA(message chan<- string) {
+func sendToA(message chan<- string) { //=> ส่งของให้A
 	time.Sleep(1 * time.Second)
 	message <- "กำลังส่งของให้A"
 }
 
 func main() {
-	start := time.Now()
+	start := time.Now() //=> Start จับเวลา
 	go buyGkassesMinimart()
 	go buyWatchesCentral()
 	ch := make(chan string)
-	go sendToA(ch)
+	go sendToA(ch) //=> อะไรซื้อแล้วส่งให้A
 	buyFruitsParagon()
 	buyCarHonda()
-	messageFromB := <-ch
+	messageFromB := <-ch //=> รับของจากB
 	if messageFromB == "กำลังส่งของให้A" {
 		fmt.Println("Aได้รับของแล้ว")
-		fmt.Println("total time: ", time.Since(start))
+		fmt.Println("total time: ", time.Since(start)) //=> ตัดเวลา
 	}
 }
 func init() {
