0c7c4128aa9f9a66eeae67e2355d1e72c48970de
tammarut <tammarut.nua@gmail.com>
2019-03-06 11:17:58 +0700

    method-pointer: try to understand how it work.

---
+++ amateur/method/method-pointer/main.go
@@ -0,0 +1,27 @@
+package main
+
+//! Method pointer
+import (
+	"fmt"
+	"math"
+)
+
+//* Normal struct
+type Paper struct {
+	X float64
+	Y float64
+}
+
+func (v *Paper) Scale(f float64) { //=>ล้วงเข้าไปแก้ค่าตัวเดียวกันได้
+	v.X *= f //=> change value on same fish
+	v.Y *= f //=> change value on same fish
+}
+func (v Paper) Abs() float64 { //=> method attach struct, it's easy to call
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	a4 := Paper{3, 4}
+	a4.Scale(10)          //=> Call method
+	fmt.Println(a4.Abs()) //=>Call and print out
+}
