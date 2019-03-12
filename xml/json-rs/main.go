64cb599852f533ac0bf766c95ba29e473f099ac9
tammarut <tammarut>
2019-03-12 10:10:19 +0700

    xml(json-rs): response json format by net/http

---
+++ xml/json-rs/main.go
@@ -0,0 +1,31 @@
+package main
+
+//! Response JSON format by net/http
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Profile struct {
+	Name    string
+	Hobbies []string
+}
+
+func main() {
+	http.HandleFunc("/", foo)         //=> path "/"
+	http.ListenAndServe(":3000", nil) //=> port, handler
+}
+
+func foo(w http.ResponseWriter, r *http.Request) { //=>handler
+	w.Header().Set("Content-Type", "application/json") //=>set header
+
+	profile := Profile{"Alex", []string{"snowboarding", "coding"}} //=> initialze data
+	js, err := json.Marshal(profile)                               //=>marshal | Go>>JSON
+	if err != nil {                                                //=>handle error
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(js)             //=> response
+	fmt.Println(string(js)) //=>print out
+}
