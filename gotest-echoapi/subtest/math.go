8e7dfe7b2b78851d28fafb68921d73f52400658f
tammarut <tammarut>
2019-03-21 15:55:16 +0700

    comment for go-lint

--- gotest-echoapi/subtest/math.go
+++ gotest-echoapi/subtest/math.go
@@ -1,14 +1,17 @@
 package subtest
 
+//Math is struct...
 type Math struct {
 	A int
 	B int
 }
 
+//Plus is regular plus...
 func (m Math) Plus() int {
 	return m.A + m.B
 }
 
+//Multiply is ...
 func (m Math) Multiply() int {
 	return m.A * m.B
 }
