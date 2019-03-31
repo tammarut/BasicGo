c1b48a06c9d6fe1547ebe0f21a15f3e6f1370dfd
tammarut <tammarut>
2019-03-31 13:49:14 +0700

    method-point: comment go-lint

--- amateur/method/method-pointer/main.go
+++ amateur/method/method-pointer/main.go
@@ -6,16 +6,19 @@ import (
 	"math"
 )
 
-//* Normal struct
+//Paper Normal struct
 type Paper struct {
 	X float64
 	Y float64
 }
 
+//Scale is Paper's method
 func (v *Paper) Scale(f float64) { //=>ล้วงเข้าไปแก้ค่าตัวเดียวกันได้
 	v.X *= f //=> change value on same fish
 	v.Y *= f //=> change value on same fish
 }
+
+//Abs ...
 func (v Paper) Abs() float64 { //=> method attach struct, it's easy to call
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
