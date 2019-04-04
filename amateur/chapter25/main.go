c6e9f1b9e5f7d33ec6c5a8ffe95acf09af965dda
tammarut <tammarut>
2019-04-04 16:18:12 +0700

    check type if it is not int  then exit

--- amateur/chapter25/main.go
+++ amateur/chapter25/main.go
@@ -4,42 +4,50 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	var keyboard string
+	var keyboard int
 	fmt.Print("Input number: ")
 	fmt.Scanln(&keyboard)
 
-	slicestring := strings.Split(keyboard, "") //.Here is key
+	if typeof(keyboard) == "int" {
+		slicestring := strings.Split(strconv.Itoa(keyboard), "") //.Here is key
 
-	for _, v := range slicestring {
-		var hold string
-		switch v {
-		case "0":
-			hold = "zero"
-		case "1":
-			hold = "one"
-		case "2":
-			hold = "two"
-		case "3":
-			hold = "three"
-		case "4":
-			hold = "four"
-		case "5":
-			hold = "five"
-		case "6":
-			hold = "six"
-		case "7":
-			hold = "seven"
-		case "8":
-			hold = "eight"
-		case "9":
-			hold = "nine"
-		default:
-			fmt.Println("Don't know!")
+		for _, v := range slicestring {
+			var hold string
+			switch v {
+			case "0":
+				hold = "zero"
+			case "1":
+				hold = "one"
+			case "2":
+				hold = "two"
+			case "3":
+				hold = "three"
+			case "4":
+				hold = "four"
+			case "5":
+				hold = "five"
+			case "6":
+				hold = "six"
+			case "7":
+				hold = "seven"
+			case "8":
+				hold = "eight"
+			case "9":
+				hold = "nine"
+			default:
+				fmt.Println("Don't know!")
+			}
+			fmt.Printf("%s ", hold)
 		}
-		fmt.Printf("%s ", hold)
+	} else {
+		fmt.Println("Please key number!")
 	}
+}
+func typeof(v interface{}) string {
+	return fmt.Sprintf("%T", v)
 }
