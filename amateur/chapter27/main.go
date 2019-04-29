d4a35dc0ae1f9105c53284271a1745180d6f67a7
tammarut <tammarut>
2019-04-29 14:45:37 +0700

    chapter27: correctly answer

--- amateur/chapter27/main.go
+++ amateur/chapter27/main.go
@@ -1,24 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 //!เขียนโปรแกรมสี่เหลี่ยมจตุรัสพื้นหลังเลขเรียง 0 ถึง รับจากคีย์บอร์ด (ถ้าเลข>10 แสดงหลักหน่วยเท่านั้น)
 
 func main() {
-	n := 2
-	pattern := (2 * n) + 1
+	var absi, absj float64
+	n := 13
+	n = n % 10
 
-	for i := 0; i < pattern; i++ {
-		for j := 0; j < pattern; j++ {
-			if i == 0 || i == pattern-1 || j == 0 || j == pattern-1 {
-				fmt.Print(n - n)
-			} else if i == (pattern-1)/2 && j == (pattern-1)/2 {
-				fmt.Print(n)
+	//. Solution2
+	for i := 0; i <= 2*n; i++ {
+		for j := 0; j <= 2*n; j++ {
+			absi = float64(n) - math.Abs((float64(n - i)))
+			absj = float64(n) - math.Abs((float64(n - j)))
+
+			if absi < absj {
+				fmt.Print(absi)
 			} else {
-				fmt.Print(n - 1)
+				fmt.Print(absj)
 			}
 		}
 		fmt.Println()
 	}
 
+	//. Solution1 >>Hard code
+	// for i := 0; i < n; i++ {
+	// 	for j := 0; j < n; j++ {
+	// 		if i == 0 || i == n-1 || j == 0 || j == n-1 {
+	// 			fmt.Print(n - n)
+	// 		} else if i == (n-1)/2 && j == (n-1)/2 {
+	// 			fmt.Print(n)
+	// 		} else {
+	// 			fmt.Print(n - 1)
+	// 		}
+	// 	}
+	// 	fmt.Println()
+	// }
 }
